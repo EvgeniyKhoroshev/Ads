2b0a51aa1c87c894aa96d087ea77ef1e13bfd780
EvgeniyKhoroshev <sonik1995@gmail.com>
2018-10-03 01:45:19 +0300

    base entity board added// not implemented ////comments

--- Domain/Entities/Base/BaseEntityboard.cs
+++ Domain/Entities/Base/BaseEntityboard.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities.Base
 {
     class BaseEntityBoard : BaseEntityPost
     {
-
+        /// <summary>
+        /// Название доски / Board name
+        /// </summary>
+        [Required]
+        string BoardName{ get; set; }
+        /// <summary>
+        /// Тело поста / Post body
+        /// </summary>
+        
     }
 }
