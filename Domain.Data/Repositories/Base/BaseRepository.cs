00c9a9488455f37d99defbbaf8a8bd425a5e51b8
EvgeniyKhoroshev <sonik1995@gmail.com>
2018-10-04 19:00:06 +0300

    base repository and its interface has been added

---
+++ Domain.Data/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Data.Repositories.Base
+{
+    public abstract class BaseRepository<T, Tid> : RepositoryInterfaces.Base.IRepositoryBase<T, int> 
+        where T: Entities.Base.BaseEntity
+    {
+        public BaseRepository() { }
+        public virtual void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IList<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public virtual T New(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual T SaveOrUpdate(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/RepositoryInterfaces/Base/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.RepositoryInterfaces.Base
+{
+    public interface IRepositoryBase<T, Tid> where  T : Entities.Base.EntityWithTypedIdBase<Tid>
+    {
+        void Delete(T entity);
+        T Get(Tid id);
+        IList<T> GetAll();
+        T SaveOrUpdate(T entity);
+        T New(T entity);
+    }
+}
