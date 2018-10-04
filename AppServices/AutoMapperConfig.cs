a2afe22e88a3dbd7071e9e9987aa325bd0e5571d
EvgeniyKhoroshev <sonik1995@gmail.com>
2018-10-05 02:04:58 +0300

    AMconfig added to startup

---
+++ AppServices/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using Ads.Contracts.Dto;
+using AutoMapper;
+using Domain.Entities;
+
+namespace AppServices
+{
+    public class AutoMapperConfig
+    {
+        public static void Initialize()
+        {
+
+            Mapper.Initialize(cfg => cfg.CreateMap<Advert, AdvertDto>());
+
+        }
+    }
+}
