b06b70edf65368372e29ad24b283a0df9ba21085
Владислав Торбек <user9f1c4b@gmail.com>
2018-10-04 18:38:30 +0300

    context

---
+++ Domain/Entities/AdsDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    class AdsDBContext : DbContext
+    {
+
+        public AdsDBContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
