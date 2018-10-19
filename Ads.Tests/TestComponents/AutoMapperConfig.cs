1ebb2c6eae1dfb36bddf2ee0ea7cc388e204a84c
Владислав Торбек <user9f1c4b@gmail.com>
2018-10-19 15:57:50 +0300

    One more commit

---
+++ Ads.Tests/TestComponents/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using Ads.Contracts.Dto;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Ads.Tests
+{
+    public class AutoMapperConfig
+    {
+        public static void Initialize()
+        {
+
+            Mapper.Initialize(cfg => {
+                cfg.CreateMap<Domain.Entities.Advert, AdvertDto>();
+                cfg.CreateMap<AdvertDto, Domain.Entities.Advert>();
+                cfg.CreateMap<AdvertsInfo, AdvertsInfoDto>();
+                cfg.CreateMap<AdvertsInfoDto, AdvertsInfo>();
+                cfg.CreateMap<CommentDto, Comment>();
+                cfg.CreateMap<Comment, CommentDto>();
+            });
+        }
+    }
+}
