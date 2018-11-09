32ec4d002145afa55fad500aa9eb9a2fe9cc1e39
Владислав Торбек <user9f1c4b@gmail.com>
2018-11-09 23:35:22 +0300

    city and region dropdown

---
+++ Ads.WebUI/wwwroot/js/SelectCity.js
@@ -0,0 +1,77 @@
+﻿//var cities;
+//var regions;
+//fetch_regions = function () {
+//    regions = fetch('https://localhost:44396/api/info/')
+//        .then(response => response.json())
+//        .then(json => regions = json.regions);
+//}
+//fetch_city = function () {
+//    cities = fetch('https://localhost:44396/api/info/')
+//        .then(response => response.json())
+//        .then(json => cities = json.cities)
+//        .then(() => DropdownRegions());
+//}
+//document.onloadstart = fetch_regions();
+//document.onloadstart = fetch_city();
+
+//function DropdownRegions() {
+//    var r = Array.from(regions);
+
+//    for (var i = 0, len = r.length; i < len; i++)
+//        document.getElementById('selectRegion').innerHTML += '<option class="bg-light" value="' + r[i].id + '">' + r[i].name + '</option>';
+//}
+
+//function GetCities() {
+//    var cityArray = new Array;
+//    for (var i = 0; i < cities.length; i++)
+//        cityArray.push(cities[i]);
+
+//    return cityArray;
+//}
+
+////function changeRegion() {
+////    var selectBox = document.getElementById("selectRegion");
+////    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+////    var c = GetCities();
+
+////    for (var i = 0; i < c.length; i++)
+////        if (selectedValue == c[i].regionId)
+////        document.getElementById('selectRegion').innerHTML += '<option class="bg-light" value="' + c[i].id + '">' + c[i].name + '</option>';
+////}
+
+//////let dropdown = document.getElementById('selectCity');
+//////dropdown.length = 0;
+
+//////let defaultOption = document.createElement('option');
+//////defaultOption.text = 'Choose State/Province';
+
+//////dropdown.add(defaultOption);
+//////dropdown.selectedIndex = 0;
+
+//////const url = 'https://localhost:44396/api/info/';
+
+//////fetch(url)
+//////    .then(
+//////        function (response) {
+//////            if (response.status !== 200) {
+//////                console.warn('Looks like there was a problem. Status Code: ' +
+//////                    response.status);
+//////                return;
+//////            }
+
+//////            // Examine the text in the response  
+//////            response.json().then(function (data) {
+//////                let option;
+
+//////                for (let i = 0; i < data.length; i++) {
+//////                    option = document.createElement('option');
+//////                    option.text = data[i].name;
+//////                    option.value = data[i].abbreviation;
+//////                    dropdown.add(option);
+//////                }
+//////            });
+//////        }
+//////    )
+//////    .catch(function (err) {
+//////        console.error('Fetch Error -', err);
+//////    });
--- Ads.WebUI/wwwroot/js/dropdownCategory.js
+++ Ads.WebUI/wwwroot/js/dropdownCategory.js
@@ -1,6 +1,6 @@
 ﻿var categories;
 fetch_category = function () {
-    categories = fetch('https://localhost:44396/api/info/')
+    categories = fetch('https://localhost:44396/api/info')
         .then(response => response.json())
         .then(json => categories = json.categories)
         .then(() => DropdownCategories());
@@ -8,23 +8,14 @@ fetch_category = function () {
 document.onloadstart = fetch_category();
 
 function DropdownCategories() {
-    var p = GetCategories();
+    var p = Array.from(categories);
 
-    for (var i = 0; i < p.length; i++) {
+    for (var i = 0, len = p.length; i < len; i++) {
         if (p[i].parentCategoryId == null)
             document.getElementById('selectCategories').innerHTML += '<option class="bg-light" value="' + p[i].id +'">' + p[i].name + '</option>';
         else
             document.getElementById('selectCategories').innerHTML += '<option value="' + p[i].id +'">' + p[i].name + '</option>';
     }  
-}
-
-function GetCategories() {
-
-    var categoryArray = new Array;
-    for (var i = 0; i < categories.length; i++)
-        categoryArray.push(categories[i]);
-
-    return categoryArray;
 }
 
 function changeFunc() {
