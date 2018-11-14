f6a3458745176f585993d1055449c3e48fdbdb6f
EvgeniyKhoroshev <sonik1995@gmail.com>
2018-11-14 23:37:37 +0300

    namespace/ user info

--- WebApi.Contracts/Dto/UserInfoDto.cs
+++ WebApi.Contracts/Dto/UserInfoDto.cs
@@ -4,6 +4,8 @@
     {
         public string Avatar { get; set; }
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
