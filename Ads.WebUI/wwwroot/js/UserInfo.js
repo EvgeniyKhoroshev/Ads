3e2fea0c587611fe1a9e93aa774739bc22e49eff
Владислав Торбек <user9f1c4b@gmail.com>
2018-11-17 11:08:00 +0300

    Поправил CreateUserDto, страницу регистрации

---
+++ Ads.WebUI/wwwroot/js/UserInfo.js
@@ -0,0 +1,15 @@
+﻿var user;
+fetch_user = function (id) {
+    var url = 'https://localhost:44396/api/Authorization/GetUserInfo/' + id + ''; 
+    user = fetch(url)
+        .then(response => response.json())
+        .then(json => user = json)
+        .then(() => UserInfo());
+}
+
+document.onloadstart = fetch_user(document.getElementById('UserId').value);
+
+
+function UserInfo() {
+    document.getElementById('UserInfo').innerHTML += '<a>имя</a>';
+}
